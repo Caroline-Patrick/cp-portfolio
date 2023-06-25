27b9e6c9c029fa6bfbfc0ff07b5c173db6b468c8
Caroline-Patrick <carolinespatrick@gmail.com>
2023-06-25 13:36:39 -0500

    Adjusted GA plugin info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,46 @@
+require("dotenv").config();
+const {GA_TRACKING_ID} = process.env
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+
 module.exports = {
   siteMetadata: {
     title: `Caroline Patrick's Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://carolinepatrick.dev`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
+//   plugins: [ 
+//     // {
+//   //   resolve: 'gatsby-plugin-google-gtag',
+//   //   options: {
+//   //     trackingIds: [GA_TRACKING_ID],
+//   //     pluginConfig: {
+//   //       head: true,
+//   //     },
+//   //   },
+//   // }, 
+//   "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+//     resolve: 'gatsby-source-filesystem',
+//     options: {
+//       "name": "images",
+//       "path": "./src/images/"
+//     },
+//     __key: "images"
+//   }]
+// };
+
+plugins: [
+  { // this must be loaded first in order to work
+    resolve: `gatsby-plugin-gtag`, // note this instead of gatsby-plugin-react-helmet
     options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
-};
+      trackingId: GA_TRACKING_ID,
+      head: true, // note this is TRUE and not FALSE as listed in other examples above
+      anonymize: true
+    }
+  },
+  "gatsby-plugin-react-helmet",
+  // other plugins
+]
+}
